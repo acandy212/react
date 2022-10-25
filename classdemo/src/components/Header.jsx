7ffc2514a4974fe6f8a9559201a6fba2b30fa09b
acandy212 <candies_amanda@columbusstate.edu>
2022-10-25 20:35:48 +0000

    updated header.jsx

--- classdemo/src/components/Header.jsx
+++ classdemo/src/components/Header.jsx
@@ -3,13 +3,15 @@ import React from "react";
 class Header extends React.Component{
 
     render(){
+        const { linkText, linkUrl } = this.props
+
         return(
             <header className="App-header">
                 <p>
                 Edit <code>src/App.js</code> and save to reload.
                 </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
-                Learn React
+                <a className="App-link" href={linkUrl} target="_blank" rel="noopener noreferrer" >
+                    {linkText}
                 </a>
             </header>
         )
