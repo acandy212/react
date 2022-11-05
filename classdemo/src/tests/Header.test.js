7a067f23d04338fe8441a7f94ee234a2de9fe685
acandy212 <candies_amanda@columbusstate.edu>
2022-11-05 20:18:04 +0000

    testing for header

--- classdemo/src/tests/Header.test.js
+++ classdemo/src/tests/Header.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Header from '../components/Header';
 
 test('renders active nav item', () => {
@@ -11,7 +11,12 @@ test('renders active nav item', () => {
             }
         }
     })
-  render(<Header />);
+    const props = { linkText: "search me", linkUrl: "www.google.com"}
+  render(<Header {...props}/>);
   const linkElement = screen.getByText(/and save to reload/i);
+  const count = screen.getByText(/You have clicked on the button/i);
   expect(linkElement).toBeInTheDocument();
+  expect(count).toBeInTheDocument();
+  const button = screen.getByRole('button');
+  fireEvent.click(button);
 });
